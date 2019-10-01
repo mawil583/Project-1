b49adcd0f3a7385b626c97a85e5b201943ca8356
Michael Williams <mwilliams2964@gmail.com>
2019-09-30 21:12:13 -0700

    added api key for testing

--- assets/project1.js
+++ assets/project1.js
@@ -1,1 +1,20 @@
 console.log("project");
+let eventfulKey = `fWBHtdbcHkhq4Lcr`
+
+let queryUrl = `https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?app_key=${eventfulKey}&category=music`
+// &category=music&location=$citta&sort_order=popularity
+// `http://api.eventful.com/json/events/search?app_key=${eventfulKey}&category=music`
+
+$.ajax(
+    {
+        url: queryUrl,
+        method: "GET"
+    }
+    ).then(
+        function(response) {
+            console.log(response)
+        },
+        function(error) {
+            console.log(error)
+        }
+    );
