84ff84b90ddd2ef296ec324a25a98feba4c667bd
Michael Williams <mwilliams2964@gmail.com>
2019-10-03 21:32:04 -0700

    reconfigured ajax call

--- assets/project1.js
+++ assets/project1.js
@@ -4,8 +4,11 @@ $(document).ready(function() {
 console.log("project");
 let eventfulKey = `fWBHtdbcHkhq4Lcr`
 
-let queryUrl = `https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/categories/list?app_key=${eventfulKey}`
-//let queryUrl = `https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?app_key=${eventfulKey}&category=outdoor&location=Phoenix`
+// let queryUrl = `https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/categories/list?app_key=${eventfulKey}`
+let queryUrl = `https://cors-anywhere.herokuapp.com/http://api.eventful.com/json/events/search?app_key=${eventfulKey}&location=Phoenix&sort_order=popularity&date=This Week`;
+
+// &category=outdoor
+
 // &category=music&location=$citta&sort_order=popularity
 // `http://api.eventful.com/json/events/search?app_key=${eventfulKey}&category=music`
 
@@ -16,7 +19,7 @@ $.ajax(
     }
     ).then(
         function(response) {
-            console.log(JSON.parse(response))
+            console.log(JSON.parse(response))  
         },
         function(error) {
             console.log(error)
