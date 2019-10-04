2b8e0a85caa4a93b3509d16d4e500db3afda7452
Eric Figueroa <ericleefigueroa@gmail.com>
2019-10-03 19:52:42 -0700

    adding js

--- assets/project1.js
+++ assets/project1.js
@@ -1,3 +1,6 @@
+$(document).ready(function() {
+    console.log("project");
+
 console.log("project");
 let eventfulKey = `fWBHtdbcHkhq4Lcr`
 
@@ -19,3 +22,43 @@ $.ajax(
             console.log(error)
         }
     );
+
+
+    
+        var cityCountry = "Los Angeles,us";
+    
+        var APIKey = "bc835c03fbfeab5d3660a9a497ae24d0";
+    
+        var queryWeatherURL = "https://api.openweathermap.org/data/2.5/forecast?" +
+        "q=" + cityCountry +"&units=imperial&appid=" + APIKey;
+    
+      
+
+
+        
+        $.ajax({
+            url: queryWeatherURL,
+            method: "GET"
+    
+        })
+    
+        .then(function (response) {
+             console.log(queryWeatherURL);
+    
+             console.log(response);
+             
+             console.log(response.list[28].main.temp);
+    
+             console.log(response.list[28].dt_txt);
+    
+             console.log(response.list[28].weather[0].description);
+    
+            
+    
+             console.log(response.city.name);
+    
+             
+        })
+    })
+    
+    
